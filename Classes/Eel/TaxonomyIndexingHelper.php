d820b5a3aa65d0173c0c57f96ce6ea9cd1cf45ca
Martin Ficzel <ficzel@sitegeist.de>
2017-12-06 18:45:45 +0100

    FEATURE: Implement taxonomy based search
    query = ${TaxonomySearch.query(site).fulltext(this.searchTerm).nodeType('Neos.Neos:Document')}

---
+++ Classes/Eel/TaxonomyIndexingHelper.php
@@ -0,0 +1,44 @@
+<?php
+namespace Sitegeist\Taxonomy\ElasticSearch\Eel;
+
+use Neos\Flow\Annotations as Flow;
+use Neos\Eel\FlowQuery\FlowQuery;
+use Neos\ContentRepository\Domain\Model\NodeInterface;
+use Sitegeist\Taxonomy\Service\TaxonomyService;
+use Neos\Neos\Domain\Service\ContentContext;
+use Neos\Eel\ProtectedContextAwareInterface;
+
+class TaxonomyIndexingHelper implements ProtectedContextAwareInterface
+{
+
+    /**
+     * @param NodeInterface[] $taxonomies
+     */
+    public function buildIdentifierList($taxonomies) {
+
+        if (!$taxonomies) {
+            return [];
+        }
+        $pathPrefixes = [];
+
+        foreach ($taxonomies as $taxonomy) {
+            $pathPrefixes[] = $taxonomy->getIdentifier();
+            $parent = $taxonomy->getParent();
+            while ($parent && $parent->getNodeType()->isOfType('Sitegeist.Taxonomy:Taxonomy')) {
+                $pathPrefixes[] = $parent->getIdentifier();
+                $parent = $parent->getParent();
+            }
+        }
+
+        return array_unique($pathPrefixes);
+    }
+
+    /**
+     * @param string $methodName
+     * @return bool
+     */
+    public function allowsCallOfMethod($methodName)
+    {
+        return true;
+    }
+}
---
+++ Classes/Eel/TaxonomySearchHelper.php
@@ -0,0 +1,83 @@
+<?php
+namespace Sitegeist\Taxonomy\ElasticSearch\Eel;
+
+use Neos\Flow\Annotations as Flow;
+use Flowpack\ElasticSearch\ContentRepositoryAdaptor\Eel\ElasticSearchQueryBuilder;
+use Sitegeist\Taxonomy\Service\TaxonomyService;
+
+class TaxonomySearchHelper extends ElasticSearchQueryBuilder
+{
+    /**
+     * @var TaxonomyService
+     * @Flow\Inject
+     */
+    protected $taxonomyService;
+
+    /**
+     * @var string
+     */
+    protected $taxonomyProperty = 'taxonomyReferences';
+
+    /**
+     * @param string $taxonomyProperty
+     * @return QueryBuilderInterface
+     */
+    public function taxonomyProperty($taxonomyProperty) {
+        $this->taxonomyProperty = $taxonomyProperty;
+        return $this;
+    }
+
+    /**
+     * Match the searchword against the fulltext index
+     *
+     * @param string $searchWord
+     * @return QueryBuilderInterface
+     */
+    public function fulltext($searchWord)
+    {
+        // create and execute a search for taxonimies first
+        $subQueryBuilder = new ElasticSearchQueryBuilder();
+        $taxonomyQueryResults = $subQueryBuilder->query($this->taxonomyService->getRootNode())
+            ->fulltext($searchWord)
+            ->nodeType('Sitegeist.Taxonomy:Taxonomy')
+            ->execute()
+            ->toArray();
+
+        if (count($taxonomyQueryResults) == 0) {
+            return parent::fulltext($searchWord);
+        }
+
+        $this->request->appendAtPath(
+            'query.filtered.filter.bool.must',
+            [
+                'bool' => [
+                    'should' => [
+                        [
+                            'query_string' => [
+                                'query' => $searchWord
+                            ]
+                        ],
+                        [
+                            'terms' => [
+                                $this->taxonomyProperty => array_map(function($taxonomyQueryResult) { return $taxonomyQueryResult->getIdentifier(); }, $taxonomyQueryResults)
+                            ]
+                        ]
+                    ]
+                ]
+            ]
+        );
+
+        $this->request->highlight(150, 2);
+
+        return $this;
+    }
+
+    /**
+     * @param string $methodName
+     * @return bool
+     */
+    public function allowsCallOfMethod($methodName)
+    {
+        return true;
+    }
+}
